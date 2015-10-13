db09048577040162259704e8a4ccbf2f93cdbdf9
IshwaryaGovindaraj <ish_athiraj@yahoo.co.in>
2015-10-13 19:05:15 +0530

    sign up page

--- server.js
+++ server.js
@@ -2,19 +2,19 @@
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var port  	 = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
-// configuration ===============================================================
+var redis = require("redis");
+var crypto = require('crypto');
+var Colu = require('colu');
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 
